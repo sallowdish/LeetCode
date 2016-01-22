1e1d7fe70c1ec07f5db18d7b996cce7af6915e6c
sallowdish <zhrud21@gmail.com>
2016-01-22 15:22:34 -0800

    Solve #107\n Rename directories

---
+++ 107_Binary_Tree_Level_Order_Traversal_II/tests.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from unittest import TestCase, main
+from sol1 import Solution
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Test(TestCase):
+
+    sol = None
+
+    def setUp(self):
+        self.sol = Solution()
+
+    def test0(self):
+        r = TreeNode(1)
+        self.assertEqual(self.sol.levelOrder(r), [[1]])
+
+    def test1(self):
+        r = TreeNode(1)
+        r.left = TreeNode(2)
+        r.right = TreeNode(3)
+        r.right.right = TreeNode(188)
+        self.assertEqual(self.sol.levelOrder(r), [[1],[2,3],[188]])
+
+
+
+
+
+if __name__ == "__main__":
+    # logging.basicConfig( stream=sys.stderr )
+    # logging.getLogger( "Test.testSomething" ).setLevel( logging.DEBUG )
+    main()
