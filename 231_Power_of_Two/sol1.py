0bd5d424f52a34766a7c8d78a2a48c89197b0841
Rui Zheng <zhrud21@gmail.com>
2016-02-06 00:27:34 -0800

    Solve #231

---
+++ 231_Power_of_Two/sol1.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 1:
+            return False
+        else:
+            return bin(n).count("1") == 1
